9f4795aba2305bafcd43c355d43961fb250b9c9f
K NIRMAL KUMAR <k.nirmalkumar2002@gmail.com>
2021-06-14 07:45:58 +0530

    Enabled login to OneMED
    Enabled login to OneMED using Firebase where the users can create the login id and password and login to the homepage

--- app/src/main/java/com/example/onemed1/MainActivity.java
+++ app/src/main/java/com/example/onemed1/MainActivity.java
@@ -1,21 +1,25 @@
 package com.example.onemed1;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Menu;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -24,20 +28,27 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.onemed1.databinding.ActivityMainBinding;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
-    private EditText username ;
-    private EditText password;
-    private Button login;
-    private String luser ="Admin";
-    private String lpassword="12345678";
+    EditText mEmail,mPassword;
+    Button mLoginBtn;
+    TextView mCreateBtn,forgotTextLink;
+    ProgressBar progressBar;
+    FirebaseAuth fAuth;
+//    private EditText username ;
+//    private EditText password;
+//    private Button login;
+//    private String luser ="Admin";
+//    private String lpassword="12345678";
     public String snip;
     public int sn=0;
-    int isValid = -1;
+//    int isValid = -1;
     FloatingActionButton fab;
     Spinner spinner;
     public static final String USER_NAME = "com.example.onemed1.username";
@@ -69,71 +80,124 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         NavigationUI.setupWithNavController(navigationView, navController);
         spinner = findViewById(R.id.spinner);
         spinner.setOnItemSelectedListener(this);
-        username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
-        login = findViewById(R.id.login);
+//        username = findViewById(R.id.Email);
+//        password = findViewById(R.id.password);
+//        login = findViewById(R.id.loginBtn);
+        mEmail = findViewById(R.id.Email);
+        mPassword = findViewById(R.id.password);
+        progressBar = findViewById(R.id.progressBar);
+        fAuth = FirebaseAuth.getInstance();
+        mLoginBtn = findViewById(R.id.loginBtn);
+        mCreateBtn = findViewById(R.id.createText);
         fab = findViewById(R.id.fab);
-                fab.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String urlText = "Its a sos";
-                        Toast.makeText(MainActivity.this, "Hey sending a Mail", Toast.LENGTH_SHORT).show();
-                        String[] addresses = {"k.nirmalkumar2002@gmail.com", "cst.20bcta16@silicon.ac.in"};
-                        Intent intent = new Intent(Intent.ACTION_SEND);
-                        intent.setType("*/*");
-                        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
-                        intent.putExtra(Intent.EXTRA_SUBJECT, "OneMED subject");
-                        intent.putExtra(Intent.EXTRA_TEXT, urlText);
-                        if (intent.resolveActivity(getPackageManager()) != null) {
-                            startActivity(intent);
-                        }
-                    }
-                });
-        login.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String inputname =username.getText().toString();
-                String inputpassword=password.getText().toString();
-                if(inputname.isEmpty()||inputpassword.isEmpty())
-                {
-                    Toast.makeText(MainActivity.this, "Invalid User-ID or Password", Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
-                    isValid = validate(inputname,inputpassword);
-                    if(isValid==1)
-                    {
-                        Toast.makeText(MainActivity.this, "Login Successfull", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(MainActivity.this, Homepage_activity.class);
-                                intent.putExtra(USER_NAME, inputname);
-                                startActivity(intent);
-                                isValid=0;
-                    }
-                    else if(isValid==2)
-                    {
-                        Toast.makeText(MainActivity.this, "Login Successfull", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(MainActivity.this, Homepage_Pharmacy.class);
-                        intent.putExtra(USER_NAME, inputname);
-                        startActivity(intent);
-                        isValid=0;
-                    }
-                    else if(isValid==3)
-                    {
-                        Toast.makeText(MainActivity.this, "Login Successfull", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(MainActivity.this, Homepage_Patient.class);
-                        intent.putExtra(USER_NAME, inputname);
-                        startActivity(intent);
-                        isValid=0;
-                    }
-
-                    else
-                    {
-                        Toast.makeText(MainActivity.this, "Login Unsuccessfull", Toast.LENGTH_SHORT).show();
-                    }
+                String urlText = "Its a sos";
+                Toast.makeText(MainActivity.this, "Hey sending a Mail", Toast.LENGTH_SHORT).show();
+                String[] addresses = {"k.nirmalkumar2002@gmail.com", "cst.20bcta16@silicon.ac.in"};
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("*/*");
+                intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+                intent.putExtra(Intent.EXTRA_SUBJECT, "OneMED subject");
+                intent.putExtra(Intent.EXTRA_TEXT, urlText);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
                 }
             }
         });
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email is Required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is Required.");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    mPassword.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+
+               // progressBar.setVisibility(View.VISIBLE);
+
+                // authenticate the user
+
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), Homepage_activity.class));
+                            progressBar.setVisibility(View.GONE);
+                        } else {
+                            Toast.makeText(MainActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+
+                    }
+//        login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String inputname =username.getText().toString();
+//                String inputpassword=password.getText().toString();
+//                if(inputname.isEmpty()||inputpassword.isEmpty())
+//                {
+//                    Toast.makeText(MainActivity.this, "Invalid User-ID or Password", Toast.LENGTH_SHORT).show();
+//                }
+//                else
+//                {
+//                    isValid = validate(inputname,inputpassword);
+//                    if(isValid==1)
+//                    {
+//                        Toast.makeText(MainActivity.this, "Login Successfull", Toast.LENGTH_SHORT).show();
+//                                Intent intent = new Intent(MainActivity.this, Homepage_activity.class);
+//                                intent.putExtra(USER_NAME, inputname);
+//                                startActivity(intent);
+//                                isValid=0;
+//                    }
+//                    else if(isValid==2)
+//                    {
+//                        Toast.makeText(MainActivity.this, "Login Successfull", Toast.LENGTH_SHORT).show();
+//                        Intent intent = new Intent(MainActivity.this, Homepage_Pharmacy.class);
+//                        intent.putExtra(USER_NAME, inputname);
+//                        startActivity(intent);
+//                        isValid=0;
+//                    }
+//                    else if(isValid==3)
+//                    {
+//                        Toast.makeText(MainActivity.this, "Login Successfull", Toast.LENGTH_SHORT).show();
+//                        Intent intent = new Intent(MainActivity.this, Homepage_Patient.class);
+//                        intent.putExtra(USER_NAME, inputname);
+//                        startActivity(intent);
+//                        isValid=0;
+//                    }
+//
+//                    else
+//                    {
+//                        Toast.makeText(MainActivity.this, "Login Unsuccessfull", Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//            }
+//        });
+                });
+            }
+        });
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Register.class));
+            }
+        });
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -180,16 +244,17 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         Toast.makeText(this, "Select the style you want to login on nothing selected", Toast.LENGTH_SHORT).show();
 
     }
-    private int validate(String name,String pass)
-    {
-        if(name.equals(luser) && pass.equals(lpassword)&&sn==1)
-            return 1 ;
-        else if (name.equals(luser) && pass.equals(lpassword)&&sn==2)
-            return 2;
-        else if(name.equals(luser) && pass.equals(lpassword)&&sn==3)
-            return 3;
-        else
-            return -1;
+//    private int validate(String name,String pass)
+//    {
+//        if(name.equals(luser) && pass.equals(lpassword)&&sn==1)
+//            return 1 ;
+//        else if (name.equals(luser) && pass.equals(lpassword)&&sn==2)
+//            return 2;
+//        else if(name.equals(luser) && pass.equals(lpassword)&&sn==3)
+//            return 3;
+//        else
+//            return -1;
+//
+//    }
 
-    }
 }
