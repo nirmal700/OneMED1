4e92879b8a5bac043c47b2be085a85f4548cbbd2
K NIRMAL KUMAR <k.nirmalkumar2002@gmail.com>
2021-06-15 19:09:08 +0530

    Slight Changes
    Change in organisation page to change to intent

--- app/src/main/java/com/example/onemed1/Homepage_activity.java
+++ app/src/main/java/com/example/onemed1/Homepage_activity.java
@@ -10,14 +10,23 @@ import android.widget.TextView;
 
 public class Homepage_activity extends AppCompatActivity {
     TextView textView;
+    Button mAddpatient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         textView = findViewById(R.id.textView8);
+        mAddpatient=findViewById(R.id.addPatient);
         Intent intent = getIntent();
         String name = intent.getStringExtra(MainActivity.USER_NAME);
         textView.setText("Hi! Mr.  " + name);
+        mAddpatient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Homepage_activity.this, PatientAddinfo.class);
+                startActivity(intent);
+            }
+        });
     }
 }
