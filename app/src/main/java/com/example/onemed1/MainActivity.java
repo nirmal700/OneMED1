20932e705f587797ffaf8241cb20a9a80d4742a0
K NIRMAL KUMAR <k.nirmalkumar2002@gmail.com>
2021-06-16 07:22:05 +0530

    Minor changes to login
    Bug need to press the button twice

--- app/src/main/java/com/example/onemed1/MainActivity.java
+++ app/src/main/java/com/example/onemed1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.onemed1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.widget.AdapterView;
@@ -30,6 +31,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.onemed1.databinding.ActivityMainBinding;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -37,27 +47,35 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
     EditText mEmail,mPassword;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRef;
+    EditText mEmail, mPassword;
     Button mLoginBtn;
-    TextView mCreateBtn,forgotTextLink;
+    TextView mCreateBtn, forgotTextLink;
     ProgressBar progressBar;
     FirebaseAuth fAuth;
-//    private EditText username ;
+    String oEm="",oPs="";
+    //    private EditText username ;
 //    private EditText password;
 //    private Button login;
 //    private String luser ="Admin";
 //    private String lpassword="12345678";
     public String snip;
-    public int sn=0;
-//    int isValid = -1;
+    public int sn = 0;
+
+    //    int isValid = -1;
     FloatingActionButton fab;
     Spinner spinner;
     public static final String USER_NAME = "com.example.onemed1.username";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef = mDatabase.getReference();
 
         setSupportActionBar(binding.appBarMain.toolbar);
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
@@ -90,6 +108,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         mLoginBtn = findViewById(R.id.loginBtn);
         mCreateBtn = findViewById(R.id.createText);
         fab = findViewById(R.id.fab);
+        progressBar.setVisibility(View.GONE);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -106,13 +125,12 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 }
             }
         });
+        Toast.makeText(MainActivity.this, "Sn value"+sn, Toast.LENGTH_SHORT).show();
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
-
                 if (TextUtils.isEmpty(email)) {
                     mEmail.setError("Email is Required.");
                     return;
@@ -128,23 +146,33 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                     return;
                 }
 
-               // progressBar.setVisibility(View.VISIBLE);
+                progressBar.setVisibility(View.VISIBLE);
 
                 // authenticate the user
 
-                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(MainActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), Homepage_activity.class));
-                            progressBar.setVisibility(View.GONE);
-                        } else {
-                            Toast.makeText(MainActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                            progressBar.setVisibility(View.GONE);
-                        }
+                if(sn==2&&validate_pharmacy(email,password)==2) {
+                    Intent intent = new Intent(MainActivity.this, Homepage_Pharmacy.class);
+                    startActivity(intent);
+                }
+                else if(sn==1&&validate_organisation(email,password)==1)
+                {
+                    Intent intent = new Intent(MainActivity.this, Homepage_activity.class);
+                    startActivity(intent);
+                }
+                else {
+                    fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(MainActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), Homepage_activity.class));
+                                progressBar.setVisibility(View.GONE);
+                            } else {
+                                Toast.makeText(MainActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.GONE);
+                            }
 
-                    }
+                        }
 //        login.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
@@ -189,16 +217,65 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 //                }
 //            }
 //        });
-                });
+                    });
+                }
+                Toast.makeText(MainActivity.this, "Sn value"+sn, Toast.LENGTH_SHORT).show();
             }
         });
         mCreateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),Register.class));
+                startActivity(new Intent(getApplicationContext(), Register.class));
             }
         });
     }
+    public int validate_pharmacy(String em, String ps) {
+        mRef.child(em).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                oEm=snapshot.child("id").getValue(String.class);
+                oPs = snapshot.child("password").getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(@NotNull DatabaseError error) {
+                mEmail.setError("As logging as P harmacy Password is Required.");
+                return;
+
+            }
+        });
+        if(oEm!=null&&oPs!=null) {
+            if (oEm.equals(em) && oPs.equals(ps)) {
+                return 2;
+            } else
+                return -1;
+        }
+        else
+            return -1;
+    }
+    public int validate_organisation(String em, String ps) {
+        mRef.child(em).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NotNull DataSnapshot snapshot) {
+                oEm = snapshot.child("oid").getValue(String.class);
+                oPs = snapshot.child("password").getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(@NotNull DatabaseError error) {
+            }
+        });
+
+            if (oEm.equals(em) && oPs.equals(ps)) {
+                return 1;
+            } else {
+                Toast.makeText(MainActivity.this, "oEm" + oEm, Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "oPs" + oPs, Toast.LENGTH_SHORT).show();
+                return -1;
+            }
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -217,24 +294,17 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         snip = spinner.getSelectedItem().toString();
-        if(snip.equals("Select Login-Style"))
-        {
+        if (snip.equals("Select Login-Style")) {
             Toast.makeText(this, "Select the style you want to login", Toast.LENGTH_SHORT).show();
-        }
-        else if (snip.equals("Organisation"))
-        {
-            sn=1;
+        } else if (snip.equals("Organisation")) {
             Toast.makeText(this, "You will be logged as organisation", Toast.LENGTH_SHORT).show();
-        }
-        else if(snip.equals("Pharmacy"))
-        {
-            sn=2;
+            sn = 1;
+        } else if (snip.equals("Pharmacy")) {
             Toast.makeText(this, "You will be logged as Pharmacy", Toast.LENGTH_SHORT).show();
-        }
-        else if (snip.equals("Patient"))
-        {
-            sn=3;
+            sn = 2;
+        } else if (snip.equals("Patient")) {
             Toast.makeText(this, "You will be logged as patient", Toast.LENGTH_SHORT).show();
+            sn = 3;
         }
 
     }
@@ -244,17 +314,21 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         Toast.makeText(this, "Select the style you want to login on nothing selected", Toast.LENGTH_SHORT).show();
 
     }
-//    private int validate(String name,String pass)
+
+    //    private int validate(String name,String pass)
 //    {
 //        if(name.equals(luser) && pass.equals(lpassword)&&sn==1)
 //            return 1 ;
 //        else if (name.equals(luser) && pass.equals(lpassword)&&sn==2)
 //            return 2;
-//        else if(name.equals(luser) && pass.equals(lpassword)&&sn==3)
+//       else if(name.equals(luser) && pass.equals(lpassword)&&sn==3)
 //            return 3;
 //        else
 //            return -1;
 //
 //    }
 
-}
+    }
+
+
+
