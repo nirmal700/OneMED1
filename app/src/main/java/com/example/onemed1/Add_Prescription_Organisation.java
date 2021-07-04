23527e5f8c7135edfa5f23b0f20a37133322ecc8
K NIRMAL KUMAR <k.nirmalkumar2002@gmail.com>
2021-07-04 20:08:38 +0530

    Working on prescribing meds to the patient
    XML has been completed, learning the recycler view and will implement soon

---
+++ app/src/main/java/com/example/onemed1/Add_Prescription_Organisation.java
@@ -0,0 +1,14 @@
+package com.example.onemed1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_Prescription_Organisation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_prescription_organisation);
+    }
+}
