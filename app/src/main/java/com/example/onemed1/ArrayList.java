ca2388394e4ced75d4a4288b4661669691ce3a4b
K NIRMAL KUMAR <k.nirmalkumar2002@gmail.com>
2021-07-06 03:58:23 +0530

    Added the PPT and the pdf of the project

---
+++ app/src/main/java/com/example/onemed1/ArrayList.java
@@ -0,0 +1,19 @@
+package com.example.onemed1;
+
+import java.util.List;
+import java.util.jar.Attributes;
+
+public class ArrayList {
+    List<String> myString ;
+    public ArrayList(List<String> myString) {
+        this.myString = myString;
+    }
+
+    public List<String> getMyString() {
+        return myString;
+    }
+
+    public void setMyString(List<String> myString) {
+        this.myString = myString;
+    }
+}
