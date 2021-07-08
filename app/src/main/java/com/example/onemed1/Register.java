a5fd5ff97c485699a30f82d2832c8aff4befdfc6
K NIRMAL KUMAR <k.nirmalkumar2002@gmail.com>
2021-07-08 23:28:26 +0530

    User Can't be created if he doesn't have a patient ID
    User Can't be created if he doesn't have a patient ID.

--- app/src/main/java/com/example/onemed1/Register.java
+++ app/src/main/java/com/example/onemed1/Register.java
@@ -17,17 +17,25 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Map;
+
 public class Register extends AppCompatActivity {
-    EditText mFullName, mEmail, mPassword, mPhone;
+    EditText mFullName, mEmail, mPassword, mPhone,mPatientID;
     Button mRegisterBtn;
     TextView mLoginBtn;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
-    FirebaseFirestore fStore;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRef,mRef1;
     String userID;
 
     @Override
@@ -40,9 +48,11 @@ public class Register extends AppCompatActivity {
         mPhone = findViewById(R.id.phone);
         mRegisterBtn = findViewById(R.id.registerBtn);
         mLoginBtn = findViewById(R.id.createText);
+        mPatientID = findViewById(R.id.editTextPatientId);
 
         fAuth = FirebaseAuth.getInstance();
-        fStore = FirebaseFirestore.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef = mDatabase.getReference("Login Info");
         progressBar = findViewById(R.id.progressBar);
 
 //        if (fAuth.getCurrentUser() != null) {
@@ -58,7 +68,7 @@ public class Register extends AppCompatActivity {
                 String password = mPassword.getText().toString().trim();
                 final String fullName = mFullName.getText().toString();
                 final String phone = mPhone.getText().toString();
-
+                final String mpid = mPatientID.getText().toString();
                 if (TextUtils.isEmpty(email)) {
                     mEmail.setError("Email is Required.");
                     return;
@@ -73,25 +83,57 @@ public class Register extends AppCompatActivity {
                     mPassword.setError("Password Must be >= 6 Characters");
                     return;
                 }
-
-                progressBar.setVisibility(View.VISIBLE);
-
-                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                if (phone.length() < 10) {
+                    mPhone.setError("Phone Must be Greater than 10");
+                    return;
+                }
+                mRef.child(mpid).addValueEventListener(new ValueEventListener() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful())
-                        {
-                            Toast.makeText(Register.this, "User Created!!", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),Homepage_activity.class));
-                        }
-                        else
-                        {
-                            Toast.makeText(Register.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                            progressBar.setVisibility(View.GONE);
-                        }
+                    public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                        mRef.child(mpid).child("Email").setValue(email);
+                        mRef.child(mpid).child("Phone").setValue(phone);
+                        mRef.child(mpid).child("Full Name").setValue(fullName);
+                        mRef.child(mpid).child("Patient ID").setValue(mpid);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull @NotNull DatabaseError error) {
+                        mEmail.setError("Registration Failed");
+                        return;
                     }
                 });
 
+                progressBar.setVisibility(View.VISIBLE);
+                mRef1=mDatabase.getReference("Patient Info");
+                mRef1.child(mpid).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                        Map<String, Object> data = (Map<String, Object>) snapshot.getValue();
+                        if (data == null) {
+                            mPatientID.setError("Patient Does't Exist");
+                            progressBar.setVisibility(View.GONE);
+                            return;
+                        } else {
+                            fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(Register.this, "User Created!!", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(getApplicationContext(), Homepage_activity.class));
+                                    } else {
+                                        Toast.makeText(Register.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+                    }
+                });
             }
         });
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
